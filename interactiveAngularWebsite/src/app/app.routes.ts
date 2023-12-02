9b44901825987fd3dbfdc3122d714e2640c59313
AmChickenTender7 <colingriggs942@gmail.com>
2023-12-01 22:01:36 -0600

    Foundation Setup and Partial NavBar

---
+++ interactiveAngularWebsite/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MenuComponent } from './pages/menu/menu.component';
+
+
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'menu',component:MenuComponent},
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
